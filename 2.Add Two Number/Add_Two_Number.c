67280d93c0c76af44b6a2cba118fba0d249333eb
oliviawu77 <jr5566i@yahoo.com.tw>
2021-04-18 16:14:27 +0800

    modify the add and carry

--- 2.Add Two Number/Add_Two_Number.c
+++ 2.Add Two Number/Add_Two_Number.c
@@ -10,57 +10,42 @@ typedef NODE *NodePtr;
 
 NodePtr addTwoNumbers(NodePtr l1, NodePtr l2){
     int carry = 0;
+    int l3_val = 0;
 
     NodePtr l3 = (NodePtr)malloc(sizeof(NODE));
-    assert(l3 != NULL);
 
     l3->next = NULL;
-    if(l1->val + l2->val < 10){
-        l3->val = l1->val + l2->val;
-        carry = 0;
-    }
-    else{
-        l3->val = (l1->val + l2->val)%10;
-        carry = 1;
-    }
 
-    NodePtr l1_current = l1;
-    NodePtr l2_current = l2;
+    l3_val = l1->val + l2->val;
+    l3->val = l3_val % 10;
+    carry = l3_val / 10;
+
     NodePtr l3_current = l3;
 
-    int l1_val, l2_val;
-
-    while(l1_current->next != NULL || l2_current->next != NULL || carry == 1){
+    while(l1->next != NULL || l2->next != NULL || carry){
 
         l3_current->next = (NodePtr)malloc(sizeof(NODE));
         l3_current = l3_current->next;
         l3_current->next = NULL; //very important!!
+        l3_current->val = 0;
 
-        if (l1_current->next != NULL){
-            l1_current = l1_current->next;
-            l1_val = l1_current->val;
-        }
-        else{
-            l1_val = 0;
+        if (l1->next != NULL){
+            l1 = l1->next;
+            l3_current->val += l1->val;
         }
 
-        if (l2_current->next != NULL){
-            l2_current = l2_current->next;
-            l2_val = l2_current->val;
-        }
-        else{
-            l2_val = 0;
+        if (l2->next != NULL){
+            l2 = l2->next;
+            l3_current->val += l2->val;
         }
 
-        if(l1_val + l2_val + carry < 10){
-            l3_current->val = l1_val + l2_val + carry;
-            carry = 0;
-        }
-        else{
-            l3_current->val = (l2_val + l1_val + carry)%10;
-            carry = 1;
+        l3_current->val += carry;
+
+        l3_val = l3_current->val;
+
+        l3_current->val = l3_current->val % 10;
+        carry = l3_val / 10;
         }
 
-    }
     return l3;
 }
